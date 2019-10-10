e466e40c8f8c6d81bcbec428ac7c8f315c94c95a
Dumi Loghin <dumiloghin@gmail.com>
2019-10-10 18:47:24 +0800

    add posit 8 and posit 16 support

--- E/E.c
+++ E/E.c
@@ -2,7 +2,7 @@
  * Compute e using Euler's formula
  */
 // #define PFDEBUG
-#define WITH_POSIT
+#define WITH_POSIT_8
 
 #include<stdint.h>
 
@@ -19,20 +19,26 @@ element_t e, k, fact;
 
 // constants
 element_t one, two;
-#ifdef WITH_POSIT
+#if defined WITH_POSIT_32
 /*
 // posit(32,2)
 uint32_t posit_zero = 0x00000000;
 uint32_t posit_one = 0x40000000;
 uint32_t posit_two = 0x48000000;
-uint32_t posit_three = 0x4c000000;
-uint32_t posit_four = 0x50000000;
 */
 
 // posit(32,3)
 uint32_t posit_zero = 0x00000000;
 uint32_t posit_one = 0x40000000;
 uint32_t posit_two = 0x44000000;
+#elif (defined WITH_POSIT_16)
+uint32_t posit_zero = 0x00000000;
+uint32_t posit_one = 0x00004000;
+uint32_t posit_two = 0x00004800;
+#elif (defined WITH_POSIT_8)
+uint32_t posit_zero = 0x00000000;
+uint32_t posit_one = 0x00000040;
+uint32_t posit_two = 0x00000050;
 #else
 uint32_t fp32_zero = 0x00000000;
 uint32_t fp32_one = 0x3f800000;
@@ -40,7 +46,7 @@ uint32_t fp32_two = 0x40000000;
 #endif /* WITH_POSIT */
 
 int main() {
-#ifdef WITH_POSIT
+#if (defined WITH_POSIT_8 || defined WITH_POSIT_16 || defined WITH_POSIT_32)
   *((uint32_t*)&one) = posit_one;
   *((uint32_t*)&two) = posit_two;
   *((uint32_t*)&e) = posit_two;
@@ -52,7 +58,7 @@ int main() {
   *((uint32_t*)&e) = fp32_two;
   *((uint32_t*)&k) = fp32_two;
   *((uint32_t*)&fact) = fp32_one;
-#endif
+#endif /* WITH_POSIT */
   int i;
   for (i = 2; i < N; i++) {
     fact = fact * k;
